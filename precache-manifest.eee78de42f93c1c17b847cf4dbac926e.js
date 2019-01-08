0447cd574284b81452098a8936c496ea788195b4
cesar angulo <acludo2@gmail.com>
2019-01-08 11:35:48 -0700

    primer commit

---
+++ precache-manifest.eee78de42f93c1c17b847cf4dbac926e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75cd58c47dae11e4bc36",
+    "url": "/static/js/main.75cd58c4.chunk.js"
+  },
+  {
+    "revision": "b82bbd196fb78f7f24f8",
+    "url": "/static/js/1.b82bbd19.chunk.js"
+  },
+  {
+    "revision": "75cd58c47dae11e4bc36",
+    "url": "/static/css/main.a1ffef55.chunk.css"
+  },
+  {
+    "revision": "ac084141483bb04a271b5df8ba501a9c",
+    "url": "/index.html"
+  }
+];
